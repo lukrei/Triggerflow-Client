4693d4f8782de78f2adcb29f72ebe12623afe6d1
lukrei <113136700+lukrei@users.noreply.github.com>
2024-04-06 14:01:38 +0200

    Update Triggerflow-Server.ps1
    better performance

--- Triggerflow-Server.ps1
+++ Triggerflow-Server.ps1
@@ -4,43 +4,56 @@ public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButt
 "@
 
 $port = 1337
-
 $udpClient = New-Object System.Net.Sockets.UdpClient($port)
 
+# Function to simulate mouse click
 function SimulateMouseClick {
-$mouseLeftButtonDown = 0x0002
-$mouseLeftButtonUp = 0x0004
-[Win32.User32]::mouse_event($mouseLeftButtonDown, 0, 0, 0, 0)
-Start-Sleep -Milliseconds 100
-[Win32.User32]::mouse_event($mouseLeftButtonUp, 0, 0, 0, 0)
+    $mouseLeftButtonDown = 0x0002
+    $mouseLeftButtonUp = 0x0004
+    [Win32.User32]::mouse_event($mouseLeftButtonDown, 0, 0, 0, 0)
+    [Win32.User32]::mouse_event($mouseLeftButtonUp, 0, 0, 0, 0)
 }
 
+# Function to handle UDP packet with timestamp
 function HandleUdpPacket {
-param (
-[byte[]]$data
-)
+    param (
+        [byte[]]$data
+    )
 
-$receivedData = [System.Text.Encoding]::ASCII.GetString($data)
-Write-Host "Received UDP packet with data: $receivedData"
+    # Emulate mouse click
+    SimulateMouseClick
 
-SimulateMouseClick
-
+    # Print received data (optional)
+    $receivedData = [System.Text.Encoding]::ASCII.GetString($data)
+    Write-Host "Received UDP packet with data: $receivedData"
 }
+
+# Asynchronously receive UDP packets
+$asyncResult = $udpClient.BeginReceive($null, $null)
 
 try {
-Write-Host "UDP Server is listening on port $port..."
+    Write-Host "UDP Server is listening on port $port..."
 
-while ($true) {
-    $remoteEndPoint = New-Object System.Net.IPEndPoint ([System.Net.IPAddress]::Any, 0)
-    $receivedData = $udpClient.Receive([ref]$remoteEndPoint)
-
-    HandleUdpPacket -data $receivedData
-}
-
+    while ($true) {
+        # Check if UDP packet has been received
+        if ($asyncResult.AsyncWaitHandle.WaitOne(0)) {
+            $remoteEndPoint = New-Object System.Net.IPEndPoint ([System.Net.IPAddress]::Any, 0)
+            $receivedData = $udpClient.EndReceive($asyncResult, [ref]$remoteEndPoint)
+            
+            # Handle UDP packet
+            HandleUdpPacket -data $receivedData
+            
+            # Restart asynchronous receive
+            $asyncResult = $udpClient.BeginReceive($null, $null)
+        } else {
+            # Sleep for a short duration to reduce CPU usage
+            Start-Sleep -Milliseconds 1
+        }
+    }
 }
 catch {
-Write-Host "Error occurred: $_.Exception.Message"
+    Write-Host "Error occurred: $_"
 }
 finally {
-$udpClient.Close()
+    $udpClient.Close()
 }
